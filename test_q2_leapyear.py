0ce3c3fba87188d89c6df241bf24fe4fa6d5ad0d
AGKirby <adamgraneto2@gmail.com>
2021-05-27 14:34:05 -0700

    Question 2: Wrote third test case, checks that years divisible by 4, 100, and 400 are considered leap years

--- test_q2_leapyear.py
+++ test_q2_leapyear.py
@@ -10,5 +10,9 @@ class testCaseAdd(unittest.TestCase):
         result = q2_leapyear.leapyear(1900)
         self.assertEqual(result, False)
 
+    def test_leapyear3(self): #check that a year divisible by 4, 100, and 400 is considered a leap year
+        result = q2_leapyear.leapyear(2000)
+        self.assertEqual(result, True)
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
