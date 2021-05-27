91a93eb51bdc442efef2134e65b0276b1575613d
AGKirby <adamgraneto2@gmail.com>
2021-05-27 14:30:53 -0700

    Question 2: Wrote second test case, checks that a year divisible by both 4 and 100 is not considered a leap year

--- test_q2_leapyear.py
+++ test_q2_leapyear.py
@@ -2,9 +2,13 @@ import unittest
 import q2_leapyear
 
 class testCaseAdd(unittest.TestCase):
-    def test_fizzbuzz1(self): #check that the function prints numbers 1 to 100
+    def test_leapyear1(self): #check that a year divisible by 4 is considered a leap year
         result = q2_leapyear.leapyear(1904)
         self.assertEqual(result, True)
+
+    def test_leapyear2(self): #check that a year divisible by both 4 and 100 is not considered a leap year
+        result = q2_leapyear.leapyear(1900)
+        self.assertEqual(result, False)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
