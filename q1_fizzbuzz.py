470badfe3a1df543703a69cc27ae3733feb6f8a8
AGKirby <adamgraneto2@gmail.com>
2021-05-27 14:17:58 -0700

    Question 1: Updated fizzbuzz method, now prints 'Fizz' instead of the number if it is divisible by 3

--- q1_fizzbuzz.py
+++ q1_fizzbuzz.py
@@ -1,5 +1,9 @@
 def fizzbuzz():
     output = ""
     for i in range(1, 101): #numbers 1 to 100
-        output += str(i) + '\n'
+        if(i % 3 == 0): #multiple of 3          step 2
+            output += "Fizz"
+        else: 
+            output += str(i) 
+        output += '\n'
     return output
