d4b5f9279d174e6e09b6e745f23cc420a4bebbec
AGKirby <adamgraneto2@gmail.com>
2021-05-27 14:32:39 -0700

    Question 2: Updated leapyear method, years divisible by both 4 and 100 are no longer considered leap years

--- q2_leapyear.py
+++ q2_leapyear.py
@@ -1,4 +1,5 @@
 def leapyear(year: int):
     if(year % 4 == 0): #divisible by 4
-        return True
+        if(year % 100 != 0): #but not divisible by 100
+            return True
     return False
