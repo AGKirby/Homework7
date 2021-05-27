e44094d08ca91e4f77bae9722deabfe61382efe4
AGKirby <adamgraneto2@gmail.com>
2021-05-27 14:24:51 -0700

    Question 1: Updated fizzbuzz method, now prints 'Buzz' instead of the number if it is divisible by 5

--- q1_fizzbuzz.py
+++ q1_fizzbuzz.py
@@ -1,9 +1,12 @@
 def fizzbuzz():
     output = ""
     for i in range(1, 101): #numbers 1 to 100
-        if(i % 3 == 0): #multiple of 3          step 2
-            output += "Fizz"
-        else: 
-            output += str(i) 
-        output += '\n'
+        append = ""
+        if(i % 3 == 0): #multiple of 3
+            append += "Fizz"
+        if(i % 5 ==0): #multiple of 5
+            append += "Buzz"
+        if(append == ""): 
+            append += str(i)  
+        output += append + '\n'
     return output
